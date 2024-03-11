30fa1375a4de53d993d46461314a5de7a75c9230
PedroHenriqueSPadilha <pedrohenriquespadilha@gmail.com>
2024-03-11 19:16:45 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    struct Pessoa
+    {
+        public string nome;
+        public double peso;
+        public double altura;
+        public double imc;
+    }
+
+    static double CalcularIMC(double peso, double altura)
+    {
+        return peso / (altura * altura);
+    }
+
+    static void Main(string[] args)
+    {
+        Pessoa pessoa;
+        int obesos = 0, abaixos = 0, acimas = 0, normais = 0;
+        double imcSoma = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Clear();
+            Console.Write($"Digite o nome {i+1}: ");
+            pessoa.nome = Console.ReadLine();
+
+            Console.Write($"Digite a altura: ");
+            pessoa.altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write($"Digite o peso: ");
+            pessoa.peso = Convert.ToDouble(Console.ReadLine());
+
+            pessoa.imc = CalcularIMC(pessoa.peso, pessoa.altura);
+
+            imcSoma += pessoa.imc;
+
+            if (pessoa.imc < 18.5)
+                abaixos++;
+
+            if (pessoa.imc > 18.5 && pessoa.imc < 24.9)
+                normais++;
+
+            if (pessoa.imc > 25 && pessoa.imc < 39.9)
+                acimas++;
+
+            if (pessoa.imc > 40)
+                obesos++;
+
+            Console.WriteLine();
+        }
+
+        Console.Clear() ;
+
+        Console.WriteLine($"Abaixo Do Peso: {abaixos}");
+        Console.WriteLine($"Normal: {normais}");
+        Console.WriteLine($"Acima do Peso: {acimas}");
+        Console.WriteLine($"Obesos:{obesos}");
+    }
+}
---
+++ Atividade4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace Atividade4
+    {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int obesos = 0, normais = 0, abaixo = 0, acimaPeso = 0;
+            float[] p = new float[5];
+            float[] a = new float[5];
+            float[] imc = new float[5];
+            string[] nome = new string[5];
+
+
+            for (int i = 0; i < 5; i++) {
+
+                Console.Clear();
+
+                Console.Write("Digite o nome: ");
+                nome[i] = Console.ReadLine();
+
+                Console.Write("Digite sua altura: ");
+                a[i] = float.Parse(Console.ReadLine());
+
+                Console.Write("Digite seu Peso: ");
+                p[i] = float.Parse(Console.ReadLine());
+
+                imc[i] = p[i] / (a[i] * a[i]);
+            }
+
+            Console.Clear();
+
+            for(int i = 0;i < 5; i++)
+            {
+                if (imc[i] < 18.5) {
+
+                    abaixo++;
+                }
+                if (imc[i] > 18.5 && imc[i] < 24.9){
+
+                    normais++;
+                }
+                if (imc[i] > 25.00 && imc[i] < 39.9){
+
+                    acimaPeso++;
+                }
+                if (imc[i] > 40) {
+
+                    obesos++;
+                }
+
+                Console.WriteLine(nome[i]);
+                Console.WriteLine($"Seu Imc é: {imc[i].ToString("00.0")}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Número de pessoas abaixo do peso: {abaixo}");
+            Console.WriteLine($"Número de pessoas com peso normal: {normais}");
+            Console.WriteLine($"Número de pessoas acima do peso: {acimaPeso}");
+            Console.WriteLine($"Número de pessoas obesas: {obesos}");
+            Console.WriteLine("");
+        }
+
+    }
+    }
